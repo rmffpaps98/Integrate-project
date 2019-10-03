3e8aa2b63d4141cbd60a26f15e95877457d80375
clemen <rmffpaps98@gmail.com>
2019-10-03 23:57:38 +0900

    j.2

---
+++ WatchDogs/app/src/main/java/kr/ac/gachon/watchdogs/DetailDict.kt
@@ -0,0 +1,12 @@
+package kr.ac.gachon.watchdogs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailDict : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_dict)
+    }
+}
