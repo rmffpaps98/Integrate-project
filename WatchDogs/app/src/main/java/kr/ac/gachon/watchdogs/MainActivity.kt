44bdb6958c7fcbdf7c6f4a33d961fd8f249f9c89
clemen <rmffpaps98@gmail.com>
2019-10-03 23:31:52 +0900

    J.1

--- WatchDogs/app/src/main/java/kr/ac/gachon/watchdogs/MainActivity.kt
+++ WatchDogs/app/src/main/java/kr/ac/gachon/watchdogs/MainActivity.kt
@@ -1,16 +1,57 @@
 package kr.ac.gachon.watchdogs
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var permissionList = arrayOf(   // 카메라, 저장소 권한 설정_j
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+    private lateinit var textMessage: TextView
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.Gallery -> {
+                textMessage.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.Camera -> {
+                textMessage.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.Dictionary -> {
+                textMessage.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        textMessage = findViewById(R.id.message)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        checkPermission()   //권한설정_j
     }
-//
-    //
-    //
-	// ...
+
+    fun checkPermission() { // 권한 설정 함수_j
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M) { return }
+        for (pms: String in permissionList) {
+            var permission_chk = checkSelfPermission(pms)
+
+            if (permission_chk == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(permissionList, 0)
+                break
+            }
+        }
+    }
 }
