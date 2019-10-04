fa895821978679773e51f97639757743cb21fa11
iwryu <dlfdnd96@gmail.com>
2019-10-04 11:10:51 +0900

    Project 이름 변경함. README.md 추가함(프로젝트 View를 Android -> Project로 변경한 후 확인 가능)

---
+++ SearchDogs/app/src/main/java/kr/ac/gachon/searchdogs/DetailDict.kt
@@ -0,0 +1,13 @@
+package kr.ac.gachon.searchdogs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.gachon.searchdogs.R
+
+class DetailDict : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_dict)
+    }
+}
