0267d1104d6ada4af56b56009cac265e578ef047
rmffpaps98 <56065194+rmffpaps98@users.noreply.github.com>
2019-11-07 00:10:50 +0900

    Update DogImageActivity.kt

--- SearchDogs/app/src/main/java/kr/ac/gachon/searchdogs/activity/DogImageActivity.kt
+++ SearchDogs/app/src/main/java/kr/ac/gachon/searchdogs/activity/DogImageActivity.kt
@@ -89,6 +89,37 @@ class DogImageActivity : AppCompatActivity() {
         val uri = Uri.parse(bitmapStringURI)
         val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
 
+        // 19.11.06 갤러리  소켓 통신 구현 - 이정묵
+        val path = getRealPathFromURI(uri)
+        val file = File(path)
+        val Comp = Compressor(this).compressToFile(file)
+        val content = Files.readAllBytes(Comp.toPath())
+        val ip = "121.169.158.111" // 192.168.0.0
+        val port = 7070 // 여기에 port를 입력해주세요
+
+        val socket = Socket(ip, port) // ip와 port를 입력하여 클라이언트 소켓을 만듭니다.
+        val outStream = socket.outputStream // outputStream - 데이터를 내보내는 스트림입니다.
+        val inStream = socket.inputStream // inputStream - 데이터를 받는 스트림입니다.
+
+        val data1 = content.size.toString().toByteArray() + ":".toByteArray() + content// 데이터는 byteArray로 변경 할 수 있어야 합니다.
+
+        outStream.write(data1) // toByteArray() 파라미터로 charset를 설정할 수 있습니다. 기본값 utf-8
+
+        val available = inStream.available() // 데이터가 있으면 데이터의 사이즈 없다면 -1을 반환합니다.
+        if (available > 0) {
+            val dataArr = ByteArray(available) // 사이즈에 맞게 byte array를 만듭니다.
+            outStream.write(dataArr) // byte array에 데이터를 씁니다.
+            val data1 = String(dataArr) // byte array의 데이터를 통해 String을 만듭니다.
+            println("data : ${data1}")
+        }
+
+        val byteArr = ByteArray(1024)
+        val ins = socket.getInputStream()
+        val readByteCount = ins.read(byteArr)
+        val msg = String(byteArr, 0, readByteCount)
+        val test = Uri.parse("$msg").toString()
+        resultView?.text = test
+
         mImageView?.setImageBitmap(bitmap)
     }
 
@@ -105,6 +136,7 @@ class DogImageActivity : AppCompatActivity() {
     private fun showTakePhotoImage() {
         val bitmapStringURI = intent.getStringExtra(INTENT_CAMERA_TAG)
         val rotatedBitmapURI = getRightAngleImage(bitmapStringURI, cameraStateData)
+        
         // 19.11.06 사진촬영한 것 소켓 통신 구현 - 이정묵
         val rotatedURI = Uri.parse(rotatedBitmapURI)
         val file = File(rotatedBitmapURI)
